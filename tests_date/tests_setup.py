9eb64d52103f9519d7bf116456f7fe8c1f143215
evgeniakalinina <evg5408@yandex.ru>
2021-02-27 09:17:52 +0300

    тесты+ / 27.02.21

--- tests_date/tests_setup.py
+++ tests_date/tests_setup.py
@@ -1,14 +1,12 @@
 import pytest
 from Date.Date import Date, TimeDelta
 
-#from Date.Date import Date, TimeDelta
-
 
 @pytest.mark.parametrize("day,month,year,expected", [(1, 12, 2020, "01.12.2020")])
 def test_create_date(day, month, year, expected):
     date = Date(day, month, year)
-    print(date.day)
     assert str(date) == expected
+
 
 @pytest.mark.parametrize("date, delta, expected", [
     ("20.07.2002", (1, 0, 0), "21.07.2002"),
@@ -20,6 +18,7 @@ def test_time_delta(date, delta, expected):
     new_d = d + t
     assert new_d.__str__() == expected
 
+
 @pytest.mark.parametrize("date1, date2, expected", [
     ("20.07.2002", "20.07.2002", 0),
     ("21.07.2002", "20.07.2002", 1)
@@ -30,6 +29,5 @@ def test_sub(date1, date2, expected):
     d3 = d2 - d1
     assert d3 == expected
 
-
-#if __name__ == '__main__':
-    #test_sub(1, 12, 2021)
+# if __name__ == '__main__':
+# test_sub(1, 12, 2021)
